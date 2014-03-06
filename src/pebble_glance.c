91f2fec611ad3a201c2d00d7ded4ef2e7039935e
nuthinking <christian@nuthinking.com>
2014-03-06 16:00:47 +0000

    Updated to Sdk2 and more
    Added also shake-for-details feature.

--- src/pebble_glance.c
+++ src/pebble_glance.c
@@ -1,23 +1,14 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-#include "resource_ids.auto.h"
-
-
-#define MY_UUID { 0x43, 0xA1, 0xE7, 0x39, 0xC3, 0xE4, 0x4D, 0xBC, 0xAC, 0x9C, 0x00, 0x9A, 0x23, 0x5E, 0x76, 0x33 }
-PBL_APP_INFO(MY_UUID,
-             "Glance", "Nuthinking",
-             1, 0, /* App version */
-             RESOURCE_ID_IMAGE_MENU_ICON,
-             APP_INFO_WATCH_FACE);
+static Window *window;
+static GBitmap *greyTexture;
 			 
 #define CX 72
 #define CY 84
 #define W 144
 #define H 168
 			 
-#define NUM_SHAPES 360
+#define DETAIL_BOX_PADDING 10
 			 
 const GPoint radialPoints[] = {
 	{72, 0},
@@ -382,99 +373,88 @@ const GPoint radialPoints[] = {
 	{71, 0}
 };
 
-GPath shapes[NUM_SHAPES];
-GPathInfo pathInfos[NUM_SHAPES];
-GPoint points[NUM_SHAPES][7];
+static Layer *graphicLayer;
 
-Window window;
+static Layer *dateLayer;
+static TextLayer *dateTextLayer;
 
-Layer graphicLayer;
+static Layer *timeLayer;
+static TextLayer *timeTextLayer;
 
-HeapBitmap greyTexture;
+static int day_phase = 0;
+static unsigned int angle;
 
-int day_phase = 0;
-unsigned int angle;
+static char time_text[] = "00:00:00";
+static char date_text[] = "Sat 16 Jun";
 
+static AppTimer *hideDetailsTimer;
+static bool detailsAreVisible = false; 
 
-PblTm currentTime;
-
-void init_shapes()
+static GPath* create_path_with_angle(int a)
 {
-	for(int i=0; i<NUM_SHAPES; i++){
-		GPathInfo info = pathInfos[i];
-		GPoint *pts = points[i];
-		
-		if(i<=90){
-			info.num_points = 4;
-			pts[0] = GPoint(CX,0);
-			pts[1] = GPoint(CX, CY);
-			pts[2] = radialPoints[i];
-			pts[3] = GPoint(W, 0);
-		}else if(i<=180){
-			info.num_points = 5;
-			pts[0] = GPoint(CX,0);
-			pts[1] = GPoint(CX, CY);
-			pts[2] = radialPoints[i];
-			pts[3] = GPoint(W, H);
-			pts[4] = GPoint(W, 0);
-		}else if(i<=270){
-			info.num_points = 6;
-			pts[0] = GPoint(CX,0);
-			pts[1] = GPoint(CX, CY);
-			pts[2] = radialPoints[i];
-			pts[3] = GPoint(0, H);
-			pts[4] = GPoint(W, H);
-			pts[5] = GPoint(W, 0);
-		}else{
-			info.num_points = 7;
-			pts[0] = GPoint(CX,0);
-			pts[1] = GPoint(CX, CY);
-			pts[2] = radialPoints[i];
-			pts[3] = GPoint(0, 0);
-			pts[4] = GPoint(0, H);
-			pts[5] = GPoint(W, H);
-			pts[6] = GPoint(W, 0);
-		}
-		info.points = pts;
-		gpath_init(&shapes[i], &info);
+	GPathInfo info;
+	if(a<=90){
+		info.num_points = 4;
+		info.points = (GPoint []) {{CX,0}, {CX, CY}, radialPoints[a], {W, 0}};
+	}else if(a<=180){
+		info.num_points = 5;
+		info.points = (GPoint []) {{CX,0}, {CX, CY}, radialPoints[a], {W, H}, {W, 0}};
+	}else if(a<=270){
+		info.num_points = 6;
+		info.points = (GPoint []) {{CX,0}, {CX, CY}, radialPoints[a], {0, H}, {W, H}, {W, 0}};
+	}else{
+		info.num_points = 7;
+		info.points = (GPoint []) {{CX,0}, {CX, CY}, radialPoints[a], {0, 0}, {0, H}, {W, H}, {W, 0}};
 	}
+	return (GPath*) gpath_create(&info);
 }
 
-void update_graphic_layer(Layer *me, GContext* ctx)
+static void update_graphic_layer(Layer *me, GContext* ctx)
 {
-	GPath path = shapes[angle];
-	GRect destination = greyTexture.bmp.bounds;
+	GPath *path = create_path_with_angle(angle);
+	GRect destination = greyTexture->bounds;
 	
 	if(day_phase == 0){
 		// midnght to 8am - black on grey
-		graphics_draw_bitmap_in_rect(ctx, &greyTexture.bmp, destination);
+		graphics_draw_bitmap_in_rect(ctx, greyTexture, destination);
 		graphics_context_set_fill_color(ctx, GColorBlack);
-		gpath_draw_filled(ctx, &path);
+		gpath_draw_filled(ctx, path);
 	}else if(day_phase == 1){
 		// 8 am to 4pm - white on black
 		graphics_context_set_fill_color(ctx, GColorBlack);
 		graphics_fill_rect(ctx, destination, 0, GCornerNone);
 		
 		graphics_context_set_fill_color(ctx, GColorWhite);
-		gpath_draw_filled(ctx, &path);
+		gpath_draw_filled(ctx, path);
 		
 	}else{
 		// 4pm to midnight - grey on white
 		graphics_context_set_fill_color(ctx, GColorBlack);
-		gpath_draw_filled(ctx, &path);
+		gpath_draw_filled(ctx, path);
 		
 		graphics_context_set_compositing_mode(ctx, GCompOpOr);
-		graphics_draw_bitmap_in_rect(ctx, &greyTexture.bmp, destination);
+		graphics_draw_bitmap_in_rect(ctx, greyTexture, destination);
 		
 	}
+	gpath_destroy(path);
+}
+
+static void update_time_layer(Layer *layer, GContext *ctx) {
+    GRect bounds = layer_get_bounds(layer);
+
+    // Draw a black filled rectangle with sharp corners
+    graphics_context_set_fill_color(ctx, GColorBlack);
+    graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+	
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    graphics_fill_rect(ctx, GRect(1,1,bounds.size.w-2, bounds.size.h-2), 0, GCornerNone);
 }
 
 // Called once per second
-void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t)
+static void display_time(struct tm *tick_time)
 {
-	get_time(&currentTime);
-	
-	int hours = currentTime.tm_hour;
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Display time");
+	int hours = tick_time->tm_hour;
 	
 	if(hours<8){
 		day_phase = 0;
@@ -485,44 +465,182 @@ void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t)
 	}
 	
 	angle = (hours % 8)*45; // add minutes
-	angle += (currentTime.tm_min*45)/60;
+	angle += (tick_time->tm_min*45)/60;
 
-	layer_mark_dirty(&graphicLayer);
+	layer_mark_dirty(graphicLayer);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Layer made dirty");
 }
 
-void handle_init(AppContextRef ctx)
+static void update_time_text(struct tm *tick_time)
 {
-	init_shapes();
+	strftime(time_text, sizeof(time_text), "%T", tick_time);
+	text_layer_set_text(timeTextLayer, time_text);
+}
+
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  display_time(tick_time);
+}
+
+static void handle_seconds_tick(struct tm *tick_time, TimeUnits units_changed) {
+  update_time_text(tick_time);
+}
+
+static void window_load(Window *window)
+{
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Window loaded");
 	
-	resource_init_current_app(&APP_RESOURCES);
-	heap_bitmap_init(&greyTexture, RESOURCE_ID_GREY_PATTERN);
-
-	window_init(&window, "Glance watch");
-	window_stack_push(&window, true /* Animated */);
-
-	window_set_background_color(&window, GColorWhite);
-
-	layer_init(&graphicLayer, GRect(0,0, window.layer.frame.size.w, window.layer.frame.size.h));
-	graphicLayer.update_proc = update_graphic_layer;
-	layer_add_child(&window.layer, &graphicLayer);
-
-	handle_second_tick(ctx, NULL);
+	graphicLayer = layer_create(GRect(0,0, layer_get_frame(window_get_root_layer(window)).size.w, layer_get_frame(window_get_root_layer(window)).size.h));
+	layer_set_update_proc(graphicLayer, update_graphic_layer);
+	layer_add_child(window_get_root_layer(window), graphicLayer);
+	
+	timeLayer = layer_create(GRect(26, 168, 92, 26));
+    layer_set_update_proc(timeLayer, update_time_layer);
+	layer_add_child(window_get_root_layer(window), timeLayer);
+	
+	timeTextLayer = text_layer_create((GRect) { {0,-6}, {92,30} });
+	text_layer_set_font(timeTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(timeTextLayer, GTextAlignmentCenter);
+	text_layer_set_background_color(timeTextLayer, GColorClear);
+	layer_add_child(timeLayer, text_layer_get_layer(timeTextLayer));
+	
+	dateLayer = layer_create(GRect(16, -26, 112, 26));
+	layer_set_update_proc(dateLayer, update_time_layer);
+	layer_add_child(window_get_root_layer(window), dateLayer);
+	
+	dateTextLayer = text_layer_create((GRect) { {0,-6}, {112,30} });
+	text_layer_set_font(dateTextLayer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(dateTextLayer, GTextAlignmentCenter);
+	text_layer_set_background_color(dateTextLayer, GColorClear);
+	layer_add_child(dateLayer, text_layer_get_layer(dateTextLayer));
+	
+    time_t now = time(NULL);
+    struct tm *tick_time = localtime(&now);
+    display_time(tick_time);
 }
 
-void handle_deinit(AppContextRef ctx)
+static void window_unload(Window *window)
 {
-  heap_bitmap_deinit(&greyTexture);
+	// destroy objects
+	animation_unschedule_all();
+	
+	layer_destroy(graphicLayer);
+	text_layer_destroy(dateTextLayer);
+	layer_destroy(dateLayer);
+	text_layer_destroy(timeTextLayer);
+	layer_destroy(timeLayer);
+}
+
+static void show_details()
+{
+	GRect from_frame = layer_get_frame(timeLayer);
+	GRect to_frame = GRect(26, H-DETAIL_BOX_PADDING-26, 92, 26);
+	PropertyAnimation *timeAnimation = property_animation_create_layer_frame(timeLayer, &from_frame, &to_frame);
+	animation_set_duration((Animation*) timeAnimation, 300);
+	animation_set_curve((Animation*) timeAnimation, AnimationCurveEaseOut);
+	animation_schedule((Animation*) timeAnimation);
+	
+	from_frame = layer_get_frame(dateLayer);
+	to_frame = GRect(16, DETAIL_BOX_PADDING, 112, 26);
+	timeAnimation = property_animation_create_layer_frame(dateLayer, &from_frame, &to_frame);
+	animation_set_duration((Animation*) timeAnimation, 300);
+	animation_set_curve((Animation*) timeAnimation, AnimationCurveEaseOut);
+	animation_schedule((Animation*) timeAnimation);
+}
+
+static void details_hidden(struct Animation *animation, bool finished, void *context)
+{
+	if(finished){
+		detailsAreVisible = false;
+	}
 }
 
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-	.deinit_handler = &handle_deinit,
-    .tick_info = {
-      .tick_handler = &handle_second_tick,
-      .tick_units = MINUTE_UNIT
-    }
-  };
-  app_event_loop(params, &handlers);
+static void hide_details(void *data)
+{
+	GRect from_frame = layer_get_frame(timeLayer);
+	GRect to_frame = GRect(26, 168, 92, 26);
+	PropertyAnimation *timeAnimation = property_animation_create_layer_frame(timeLayer, &from_frame, &to_frame);
+	animation_set_duration((Animation*) timeAnimation, 500);
+	animation_set_curve((Animation*) timeAnimation, AnimationCurveEaseIn);
+	animation_schedule((Animation*) timeAnimation);
+	
+	from_frame = layer_get_frame(dateLayer);
+	to_frame = GRect(16, -26, 112, 26);
+	timeAnimation = property_animation_create_layer_frame(dateLayer, &from_frame, &to_frame);
+	animation_set_duration((Animation*) timeAnimation, 500);
+	animation_set_curve((Animation*) timeAnimation, AnimationCurveEaseIn);
+	animation_set_handlers((Animation*) timeAnimation, (AnimationHandlers) {
+        .stopped = (AnimationStoppedHandler) details_hidden,
+      }, NULL);
+	animation_schedule((Animation*) timeAnimation);
 }
+
+static void accel_tap_handler(AccelAxisType axis, int32_t direction)
+{
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Tapped!");
+	
+	if(detailsAreVisible){
+		app_timer_reschedule(hideDetailsTimer, 5000);
+	}else{
+	    time_t now = time(NULL);
+	    struct tm *tick_time = localtime(&now);
+	    update_time_text(tick_time);
+		
+		strftime(date_text, sizeof(date_text), "%a %e %b", tick_time);
+		text_layer_set_text(dateTextLayer, date_text);
+	
+		show_details();
+	
+		tick_timer_service_subscribe(SECOND_UNIT, handle_seconds_tick);
+		hideDetailsTimer = app_timer_register(5000, hide_details, NULL);
+		detailsAreVisible = true;
+	}
+}
+
+
+
+
+static void init(void)
+{
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Init");
+	//init_shapes();
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Shapes inited");
+	
+	//resource_init_current_app(&APP_RESOURCES);
+	greyTexture = gbitmap_create_with_resource(RESOURCE_ID_GREY_PATTERN);
+	
+	window = window_create();
+	window_set_window_handlers(window, (WindowHandlers) {
+		.load = window_load,
+		.unload = window_unload,
+	});
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Window created: %p", window);
+	window_set_background_color(window, GColorWhite);
+
+	const bool animated = true;
+	window_stack_push(window, animated);
+	
+	tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	accel_tap_service_subscribe(&accel_tap_handler);
+	
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Subscribed to updates");
+}
+
+static void deinit(void)
+{
+	tick_timer_service_unsubscribe();
+	accel_tap_service_unsubscribe();
+	
+	gbitmap_destroy(greyTexture);
+	window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
+
